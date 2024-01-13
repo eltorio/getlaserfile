065a0c916b174cf4f20519c5c2f77d70e19eb37c
hcf coder <hcf@coder>
2024-01-13 17:26:46 +0000

    correct default location

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _PORT="${PORT:-80}"
-_REPOLOCATION="${REPOLOCATION:-80}"
+_REPOLOCATION="${REPOLOCATION:-/repo}"
 _IHMLOCATION="${IHMLOCATION:-builds/IHM/ihm.exe}"
 _STARTUPLOCATION="${STARTUPLOCATION:-builds/sbRIO-9651/home/lvuser/natinst/bin/startup.rtexe}"
 /usr/local/bin/getlaserfile --port=${_PORT} --repolocation=${_REPOLOCATION} --ihmlocation=${_IHMLOCATION} --startuplocation=${_STARTUPLOCATION}
