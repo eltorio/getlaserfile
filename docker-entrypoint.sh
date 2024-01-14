8a5882bd7a600a9c501f97da793ac97b7e886af8
hcf coder <hcf@coder>
2024-01-14 10:36:34 +0000

    bug: forgot to update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 _PORT="${PORT:-80}"
-_REPOLOCATION="${REPOLOCATION:-/repo}"
-_IHMLOCATION="${IHMLOCATION:-builds/IHM/ihm.exe}"
-_STARTUPLOCATION="${STARTUPLOCATION:-builds/sbRIO-9651/home/lvuser/natinst/bin/startup.rtexe}"
-/usr/local/bin/getlaserfile --port=${_PORT} --repolocation=${_REPOLOCATION} --ihmlocation=${_IHMLOCATION} --startuplocation=${_STARTUPLOCATION}
+_CONFIGLOCATION="${CONFIGLOCATION:-/etc/getlaserfile/config.yaml}"
+/usr/local/bin/getlaserfile --port=${_PORT} --config=${_CONFIGLOCATION}
